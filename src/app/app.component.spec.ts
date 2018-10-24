0db313991a06420e51203eea897ff6dd77917ecc
Julien CABALLOL <jcaballol@serial.ch>
2018-10-24 22:08:06 +0200

    Exercise 2 (List of races) finished

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { RacesComponent } from './races/races.component';
 import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
@@ -24,5 +25,13 @@ describe('AppComponent', () => {
     const element = fixture.debugElement;
     expect(element.query(By.directive(MenuComponent)))
       .not.toBeNull('You probably forgot to add MenuComponent to the AppComponent template');
+  });
+
+  it('should use the races component', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const element = fixture.debugElement;
+    expect(element.query(By.directive(RacesComponent)))
+      .not.toBeNull('You probably forgot to add RacesComponent to the AppComponent template');
   });
 });
