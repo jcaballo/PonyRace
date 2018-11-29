84f35e73aaa65a156c204652f8559c889524da7a
Julien CABALLOL <julien.caballol@hotmail.fr>
2018-11-29 14:15:51 +0100

    Exercise 14 (Display the user) finished

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { UserService } from './../user.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserModel } from '../models/user.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'pr-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
-
+export class MenuComponent implements OnInit, OnDestroy {
   navbarCollapsed = true;
+  user: UserModel;
+  userEventsSubscription: Subscription;
 
-  constructor() {}
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userEventsSubscription = this.userService.userEvents.subscribe(user => this.user = user);
   }
 
   toggleNavbar() {
     this.navbarCollapsed = !this.navbarCollapsed;
   }
+
+  ngOnDestroy(): void {
+    if (this.userEventsSubscription) {
+      this.userEventsSubscription.unsubscribe();
+    }
+  }
+
 
 }
