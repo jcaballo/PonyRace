503c7efc986ac1c63c8ef3a9c89561a1c89e2103
Julien CABALLOL <julien.caballol@hotmail.fr>
2018-12-19 13:54:44 +0100

    Exercise 19 (Bet on a pony) finished

--- src/app/bet/bet.component.ts
+++ src/app/bet/bet.component.ts
@@ -1,4 +1,8 @@
+import { PonyModel } from './../models/pony.model';
+import { ActivatedRoute } from '@angular/router';
+import { RaceModel } from './../models/race.model';
 import { Component, OnInit } from '@angular/core';
+import { RaceService } from '../race.service';
 
 @Component({
   selector: 'pr-bet',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bet.component.css']
 })
 export class BetComponent implements OnInit {
+  raceModel: RaceModel;
+  betFailed = false;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private raceService: RaceService) { }
 
   ngOnInit() {
+    const raceId = Number(this.activatedRoute.snapshot.paramMap.get('raceId'));
+    if (!isNaN(raceId)) {
+      this.raceService.get(raceId).subscribe(race => this.raceModel = race);
+    }
+  }
+
+  betOnPony(pony: PonyModel) {
+    this.raceService.bet(this.raceModel.id, pony.id).subscribe(race => this.raceModel = race, () => this.betFailed = true);
+  }
+
+  isPonySelected(pony: PonyModel): boolean {
+    return pony.id === this.raceModel.betPonyId;
   }
 
 }
