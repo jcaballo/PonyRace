9b6ebf1ee01a63be1f6c38f35730c49ccd693268
Julien CABALLOL <julien.caballol@hotmail.fr>
2019-01-05 16:45:15 +0100

    Exercise 22 (WebSockets) finished

--- src/app/models/race.model.ts
+++ src/app/models/race.model.ts
@@ -1,4 +1,4 @@
-import { PonyModel } from './pony.model';
+import { PonyModel, PonyWithPositionModel } from './pony.model';
 
 export interface RaceModel {
   id: number;
@@ -7,4 +7,8 @@ export interface RaceModel {
   startInstant: string;
   status?: string;
   betPonyId?: number;
+}
+
+export interface LiveRaceModel {
+  ponies: Array<PonyWithPositionModel>;
 }
--- src/app/ws.service.ts
+++ src/app/ws.service.ts
@@ -1,22 +1,36 @@
 import { environment } from './../environments/environment.prod';
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, Type } from '@angular/core';
 import { Observable } from 'rxjs';
+import { WEBSOCKET, WEBSTOMP } from './app.tokens';
+import { Client, Subscription } from 'webstomp-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WsService {
 
-  constructor() { }
+  constructor(@Inject(WEBSOCKET) private WebSocket: Type<WebSocket>, @Inject(WEBSTOMP) private Webstomp: any) {}
 
-  connect<T>(channel): Observable<T> {
-    return Observable.create(observer => {
-      const connection: WebSocket = new WebSocket(`${environment.wsBaseUrl}/ws`);
-       // TODO create the stomp client with Webstomp
-      // TODO connect the stomp client
-      // TODO subscribe to the specific channel and store the subscription
-      // TODO handle incoming messages
-      // TODO handle the unsubscription
+  connect<T>(channel: string): Observable<T> {
+    return Observable.create((observer: any) => {
+      const connection: WebSocket = new this.WebSocket(`${environment.wsBaseUrl}/ws`);
+      const stompClient: Client = this.Webstomp.over(connection);
+      let subscription: Subscription;
+      stompClient.connect({ login: null, passcode: null }, () => {
+        subscription = stompClient.subscribe(channel, message => {
+          const bodyAsJson = JSON.parse(message.body);
+          observer.next(bodyAsJson);
+        });          // TODO emit the message received, after extracting the JSON from the body
+      }, error => {
+        // propagate the error
+        observer.error(error);
+      });
+      return () => {
+        if (subscription) {
+          subscription.unsubscribe();
+        }
+        connection.close();
+      };
     });
   }
 }
